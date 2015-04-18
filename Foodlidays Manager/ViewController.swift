c199b77240cee9b705c1e0e4db1c5865407ccbed
timothyk0908 <timothyk0908@gmail.com>
2015-04-19 01:04:36 +0200

    Début manager

--- Foodlidays Manager/ViewController.swift
+++ Foodlidays Manager/ViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var labelEmail: UITextField!
+    @IBOutlet weak var labelPassword: UITextField!
+    
+    var infoManager : JSON!
+    
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let range = testStr.rangeOfString(emailRegEx, options:.RegularExpressionSearch)
+        let result = range != nil ? true : false
+        return result
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bgFoodlidays")!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func showAlert(msg : String)
+    {
+        let alertController = UIAlertController(title: "Error", message: "\(msg)", preferredStyle: .Alert)
+        let ok = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+        })
+        alertController.addAction(ok)
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if(segue.identifier == "goto_orders"){
+            let destinationVC = segue.destinationViewController as! OrdersVC
+            destinationVC.infoManager = self.infoManager
+        }
+    }
+    
 
+    @IBAction func signInPressed(sender: AnyObject) {
+    let alertController = UIAlertController(title: "Error", message: "Please enter a valid email", preferredStyle: .Alert)
+        
+        if self.isValidEmail(labelEmail.text)
+        {
+    
+            let parameters = [
+                "email": self.labelEmail.text,
+                "password": self.labelPassword.text
+            ]
+        
+            Alamofire.request(.POST, "http:foodlidays.dev.innervisiongroup.com/api/v1/resto/login",parameters: parameters).responseJSON() {
+                (_, _, jsonData, error) in
+            
+                if error == nil {
+                    println(jsonData)
+                    self.infoManager = JSON(jsonData!)
+                } else {
+                    println("Bad identifiers")
+                    self.showAlert("Wrong Email/Password combination")
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                }
+            }
+        } else {
+            println("Bad email")
+            self.showAlert("Please enter a correct e-mail")
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+    
+    }
 
 }
 
